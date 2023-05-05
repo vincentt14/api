fa54e90a39b615641e3b047c038e7188b4cfdb57
Vincent <vincent14@student.umn.ac.id>
2023-05-05 21:46:42 +0700

    adding AuthenticationController

--- rest-api/routes/api.php
+++ rest-api/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthenticationController;
 use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -17,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/posts', [PostController::class, 'index'])->middleware(['auth:sanctum']);
 Route::get('/posts/{id}', [PostController::class, 'show'])->middleware(['auth:sanctum']);
+
+Route::post('/login', [AuthenticationController::class, 'login']);
